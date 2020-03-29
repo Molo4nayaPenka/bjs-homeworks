1daa5aa7afaaa0b245826dd37eeefc474dc24574
Olga Medvetskaya <olgamedvetskaya@yandex.ru>
2020-03-29 18:40:11 +0300

    task 2 done

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -14,8 +14,19 @@ function getResult(a,b,c) {
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let averageMark = 0;
+    if (marks.length === 0)  {
+        return averageMark;
+    } else if (marks.length > 5) {
+        console.log("введено более 5 аргументов, лишние будут удалены");
+        marks = marks.slice(0, 5);
+    }
+    let sum = 0;
+    for (let i=0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    averageMark = sum / marks.length;
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
