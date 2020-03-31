d10f218cb06c46db5b2d86b909f9e5b1d629e5fd
Olga Medvetskaya <olgamedvetskaya@yandex.ru>
2020-03-31 22:38:13 +0300

    frst task done

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,26 @@
+"use strict"
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    // let fullYears = date.getFullYear() - new Date().getFullYear();
+    // let fullMonths = date.getMonth() - new Date().getMonth();
+    // let months = fullYears * 12 + fullMonths;
+    let months = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+    console.log(months);
+    if (isNaN(percent)) {
+        console.log(`Параметр 'Процентная ставка' содержит неправильное значение ${percent.valueOf()}`);
+    } 
+    if (isNaN(contribution)) {
+        console.log(`Параметр 'Начальный взнос' содержит неправильное значение ${contribution.valueOf()}`);
+    } 
+    if (isNaN(amount)) {
+        console.log(`Параметр 'Общая строимость' содержит неправильное значение ${amount.valueOf()}`)
+    }
+    
+    let mortgageBody = amount - contribution;
+    let P = (1 / 12 * percent) / 100;
+    let monthlyPayment = mortgageBody * (P + P / ((Math.pow((1 + P), months) - 1)));
+    let totalAmount = monthlyPayment * months;
+    totalAmount = totalAmount.toFixed(2);
+    return totalAmount;
 }
 
 function getGreeting(name) {
